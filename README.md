ebdf38c696f742f0d783d2e9e76b218128c89f74
Lucas Bickel <hairmare@rabe.ch>
2019-01-02 16:18:10 +0100

    :tada: basic single user logon plugin

---
+++ README.md
@@ -0,0 +1,6 @@
+# Roundcube Plugin Single User Autologon
+
+This plugin automatically logins in a configurable user to roundcube.
+
+It may be used if you would like to make a single email-account available to
+all users without sharing the accounts password with all users.
---
+++ singleuserautologon.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This performs an automatic login with the configured credentials.
+ *
+ * @license GNU GPLv3+
+ */
+class singleuserautologon extends rcube_plugin
+{
+  public $task = 'login';
+
+  function init()
+  {
+    $this->load_config();
+
+    $this->add_hook('startup', array($this, 'startup'));
+    $this->add_hook('authenticate', array($this, 'authenticate'));
+  }
+
+  function startup($args)
+  {
+    // change action to login
+    if (empty($_SESSION['user_id']) && !empty($_GET['_autologin']))
+      $args['action'] = 'login';
+
+    return $args;
+  }
+
+  function authenticate($args)
+  {
+    if (!empty($_GET['_autologin'])) {
+      $args['user'] = $this->get_config('username');
+      $args['pass'] = $this->get_config('password');
+      $args['host'] = $this->get_config('hostname');
+      $args['cookiecheck'] = false;
+      $args['valid'] = true;
+    }
+
+    return $args;
+  }
+
+  protected function get_config($key)
+  {
+    $rcmail = rcube::get_instance();
+
+    return $rcmail->config->get(sprintf('singleuserautologon_%s', $key));
+  }
+}
